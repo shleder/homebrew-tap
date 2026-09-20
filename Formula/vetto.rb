723ee419ff0b7bb139d55b4097811ed3f879db4a
Shleder <maksboreichuk88@gmail.com>
2026-09-20 13:05:31 +0300

    chore(formula): bump vetto to 0.3.4

--- Formula/vetto.rb
+++ Formula/vetto.rb
@@ -1,26 +1,26 @@
 class Vetto < Formula
   desc "Daemon-less OS sandbox and subagent security layer for AI coding agents"
   homepage "https://github.com/shleder/vetto"
-  version "0.3.3"
+  version "0.3.4"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/shleder/vetto/releases/download/v0.3.3/vetto-macos-aarch64.tar.gz"
-      sha256 "3f54c728ae8bd2999f30f7f466d0054f49e853f427b21067e3a89e983b1f1c75"
+      url "https://github.com/shleder/vetto/releases/download/v0.3.4/vetto-macos-aarch64.tar.gz"
+      sha256 "fbae7e4db70f5e86a923e058c052fd067c66bf8de04b94cc3349f8436e024b48"
     else
-      url "https://github.com/shleder/vetto/releases/download/v0.3.3/vetto-macos-x86_64.tar.gz"
-      sha256 "e4d270aa44997613c8a890bf32575769a437f1043ead3fd3a0f4a83e36f35db7"
+      url "https://github.com/shleder/vetto/releases/download/v0.3.4/vetto-macos-x86_64.tar.gz"
+      sha256 "a1db8d6acbdfca18d345812cd5548a9d39a46e59ecd359315b93ee69ddfa330c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/shleder/vetto/releases/download/v0.3.3/vetto-linux-aarch64.tar.gz"
-      sha256 "fb8f7bef2ee04767b0b0d097cc2f9e8f2ffe4cd26cec4901dafe4c01d8b46fa1"
+      url "https://github.com/shleder/vetto/releases/download/v0.3.4/vetto-linux-aarch64.tar.gz"
+      sha256 "cecc303723e0c367957e418eb2a952d3a1670c2bf894c762c1509ff497f25fb8"
     else
-      url "https://github.com/shleder/vetto/releases/download/v0.3.3/vetto-linux-x86_64.tar.gz"
-      sha256 "2e66e6633f4d22035b7cdac073199a955c1ddce15ad85628cc0489f9f5155024"
+      url "https://github.com/shleder/vetto/releases/download/v0.3.4/vetto-linux-x86_64.tar.gz"
+      sha256 "540532959247bb8c6ad8300e659df67953f0246d34cbbb0cd9475602b09238ad"
     end
   end
 
@@ -29,6 +29,6 @@ class Vetto < Formula
   end
 
   test do
-    assert_match "vetto", shell_output("#{bin}/vetto --version")
+    system "#{bin}/vetto", "--version"
   end
 end
